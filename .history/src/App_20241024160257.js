5f70ff0b001a86dcff29f365eae579f65d790fb5
Sandi Hermawan <sandi@quantuminventions.com>
2024-10-24 16:06:03 +0700

    init

---
+++ .history/src/App_20241024160257.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <APIProvider apiKey={API_KEY}>
+    <Map
+      style={{width: '100vw', height: '100vh'}}
+      defaultCenter={{lat: 22.54992, lng: 0}}
+      defaultZoom={3}
+      gestureHandling={'greedy'}
+      disableDefaultUI={true}
+    />
+  </APIProvider>
+  );
+}
+
+export default App;
---
+++ .history/src/App_20241024160317.js
@@ -0,0 +1,18 @@
+import './App.css';
+import {APIProvider, Map} from '@vis.gl/react-google-maps';
+
+function App() {
+  return (
+    <APIProvider apiKey={API_KEY}>
+    <Map
+      style={{width: '100vw', height: '100vh'}}
+      defaultCenter={{lat: 22.54992, lng: 0}}
+      defaultZoom={3}
+      gestureHandling={'greedy'}
+      disableDefaultUI={true}
+    />
+  </APIProvider>
+  );
+}
+
+export default App;
